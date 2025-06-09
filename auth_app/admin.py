f0bb3f88efa7849dc76269e92cbe92cb6bf26dc7
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-06-09 11:49:21 +0200

    tryed admin customview

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class CustomAdmin(admin.ModelAdmin):
+    list_filter = ["type"]
+
+admin.site.register(UserProfile , CustomAdmin)
