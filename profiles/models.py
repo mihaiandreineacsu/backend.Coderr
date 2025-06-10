80099c6510cb81d4c13a3558d626f9b783854f52
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-06-10 11:20:12 +0200

    aufbaue model Profile

--- profiles/models.py
+++ profiles/models.py
@@ -7,13 +7,51 @@ class Profile(models.Model):
     user = models.OneToOneField(
         UserProfile, on_delete=models.CASCADE, related_name="profile"
     )
+    file = models.CharField( blank=False, default="profile_picture.jpg")
+    location = models.CharField(max_length=255, blank=True, default="")
+    tel = models.CharField(max_length=30, blank=True, default="")
+    description = models.TextField(blank=True, default="")
+    working_hours = models.CharField(max_length=50, blank=True, default="")
+
+    def __str__(self):
+        return f"Profile of {self.user.user.username}"
+
+    @property
+    def username(self):
+        return self.user.user.username
 
     @property
     def first_name(self):
-        return self.user_profile.user.first_name
+        return self.user.user.first_name
 
     @property
     def last_name(self):
-        return self.user_profile.user.last_name
+        return self.user.user.last_name
 
-    # TODO: Hier fortsetzten Profile daten anlegen
+    @property
+    def email(self):
+        return self.user.user.email or ""
+
+    @property
+    def type(self):
+        return self.user.type
+
+    @property
+    def created_at(self):
+        return self.user.user.date_joined
+
+
+{
+    "user": 1,
+    "username": "max_mustermann",
+    "first_name": "Max",
+    "last_name": "Mustermann",
+    "file": "profile_picture.jpg",
+    "location": "Berlin",
+    "tel": "123456789",
+    "description": "Business description",
+    "working_hours": "9-17",
+    "type": "business",
+    "email": "max@business.de",
+    "created_at": "2023-01-01T12:00:00",
+}
