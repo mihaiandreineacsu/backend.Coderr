77b41e8985c204dfe89f1f762ac9e6d368002254
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-06-11 15:17:33 +0200

    add offerDetails model

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class OfferDetails(models.Model):
+   OFFER_TYPE_CHOICES = [
+       ('basic', 'Basic')
+   ]
+   
+   title = models.CharField(max_length=200)
+   revisions = models.PositiveIntegerField(default=0,blank=True)
+   delivery_time_in_days = models.PositiveIntegerField(default=0,blank=True)
+   price = models.DecimalField(max_digits=10, decimal_places=2 ,blank=True )
+   features = models.JSONField(default=[])
+   offer_type = models.CharField(max_length=20, choices=OFFER_TYPE_CHOICES, default='basic')
+   
+   def __str__(self):
+       return self.title
+   
+   class Meta:
+       verbose_name = "Offer Detail"
+       verbose_name_plural = "Offer Details"
